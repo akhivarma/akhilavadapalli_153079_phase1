872e682fa44992590566107ad74d17f0869e2e62
akhila_vadapalli <40975643+akhivarma@users.noreply.github.com>
2018-07-18 09:07:15 +0530

    Add files via upload

---
+++ parallelproject_153079_phase1/src/com/cg/mypaymentapp/exception/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package com.cg.mypaymentapp.exception;
+
+public class InsufficientBalanceException extends RuntimeException{
+	public InsufficientBalanceException(String msg) {
+		super(msg);
+	}
+}
